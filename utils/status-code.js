8ba44c568ac8bbdf897840e25b3aa1d09a9a452a
Saheed Ajibulu <stunnafrsh007@gmail.com>
2020-07-04 14:56:33 +0100

    add reusable statusCodes to utils

---
+++ utils/status-code.js
@@ -0,0 +1,15 @@
+const OK = 200;
+const Created = 201;
+const BadRequest = 400;
+const Unauthorized = 401;
+const NotFound = 404;
+const InternalServerErr = 500;
+
+module.exports = {
+  OK,
+  Created,
+  BadRequest,
+  Unauthorized,
+  NotFound,
+  InternalServerErr,
+};

57fea16bcf2d8045be3aa3abc287e2cc6e84883a
Saheed Ajibulu <stunnafrsh007@gmail.com>
2020-07-05 01:41:38 +0100

    - implement updateActor and getStreak

--- controllers/actors.js
+++ controllers/actors.js
@@ -1,14 +1,106 @@
+const db = require("../database");
+const { BadRequestError } = require("../utils/errors/bad-request-error");
+const { InternalServerError } = require("../utils/errors/internal-server-error");
+const { OK, Created } = require("../utils/status-code");
+const { NotFoundError } = require("../utils/errors/not-found-error");
 
-var getAllActors = () => {
-	
+const {
+  containsInvalidAttr,
+  extractDistinctActors,
+  allowedActorUpdateAttrs,
+  actorStreak,
+  sortActorByStreak,
+  removeActorStreak,
+} = require("../utils");
+
+const getAllActors = async (req, res, next) => {
+	// TODO
+	try {
+		const events = await db.find({}).sort({
+			actor: -1,
+			created_at: -1,
+			'actor.login': 1
+		}).exec()
+		// created_at: -1, "actor.login": 1
+		console.log(events);
+		const actors = extractDistinctActors(events);
+		res.status(OK).json({
+			data: actors
+		})
+	} catch (error) {
+		console.error("DatabaseOperation Error: ", error);
+    next(
+      new InternalServerError({
+        message: "Error fetching data",
+      })
+    );
+	}
 };
 
-var updateActor = () => {
+const updateActor = async (req, res, next) => {
+	try {
+		const incomingAttrs = Object.keys(req.body);
+		
+		if (containsInvalidAttr(incomingAttrs, allowedActorUpdateAttrs)) {
+      return next(
+        new BadRequestError({
+          message: "Invalid attributes supplied",
+        })
+      );
+    }
 
+		const { id, avatar_url } = req.body;
+		const found = await db.findOne({ "actor.id": id });
+
+		if (!found) {
+			return next(
+				new NotFoundError({
+					message: "Actor not found"
+				})
+			);
+		}
+		
+		const update = await db.update(
+      { "actor.id": id },
+      { $set: { "actor.avatar_url": avatar_url } },
+      { returnUpdatedDocs: true, multi: true }
+    );
+		
+		res.status(OK).json({
+			data: update
+    });
+	} catch (error) {
+		console.error("DatabaseOperation Error: ", error);
+    next(
+      new InternalServerError({
+        message: "Error fetching data",
+      })
+    );
+	}
 };
 
-var getStreak = () => {
-
+const getStreak = async (req, res, next) => {
+	try {
+		const sortEvents = await db.find({})
+			.sort({ created_at: -1, 'actor.login': 1 })
+			.exec();
+		
+		const sortByStreak = removeActorStreak(
+			sortActorByStreak(
+				actorStreak(
+					sortEvents
+				)
+			)
+		);
+		res.status(OK).json({ data: sortByStreak });
+	} catch (error) {
+		console.error("DatabaseOperation Error: ", error);
+    next(
+      new InternalServerError({
+        message: "Error fetching data",
+      })
+    );
+	}
 };
 
 
